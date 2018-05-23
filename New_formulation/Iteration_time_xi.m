9f5c1bbddc737ab1a7a83841a34f4ffd266bb032
yvesrobert195 <yvesrobert@airbears2-10-142-124-47.airbears2.1918.berkeley.edu>
2018-05-23 09:53:59 -0700

    Iteration xi

---
+++ New_formulation/Iteration_time_xi.m
@@ -0,0 +1,90 @@
+%% User Input
+fprintf('*********************************************************************\n')
+fprintf('ORIFICE ITERATION TIME SCRIPT\n')
+fprintf(datestr(now))
+fprintf('\n*********************************************************************\n')
+User_Input
+% INPUT SPECIFICALLY FOR ITERATION TIME
+comment=['_test']; %additional comments for the name (put '_')
+sol_name=['xi_' datestr(now,'mm-dd-yy_HH-MM-SS') comment '.mat']; %creates unique mat file for the test
+ini=1;
+
+%% Initialization
+fprintf('*********************************************************************\n')
+fprintf('Initialization\n')
+fprintf('*********************************************************************\n')
+% Assign geometry
+fprintf('\tReading geometry\n')
+Geometry=geometry(Input.Core);
+
+% Reads det files
+fprintf('\tReading power data\n')
+Input.Q = readQ(Input.powerDetectorFiles);
+Input.lengthQ_original = length(Input.Q);
+[Input.Q, Input.map] = formMap(Input.Q, Input.assemblyPowerThreshold);
+Input.Q_ave = sum(Input.Q,2)/length(Input.powerDetectorFiles); % divide by number of steps to get average assembly power over cycle
+
+fprintf('\tManipulating data\n');
+Pb.Var.nass = length(Input.Q_ave); % number of assemblies in problem
+Pb.Var.nsteps = length(Input.powerDetectorFiles);
+
+n=0;
+while 1
+    fprintf('\n\n*********************************************************************\n')
+    fprintf(['TEST WITH ' num2str(ini+n) ' FLOWRATES\n'])
+    fprintf('*********************************************************************\n')
+    
+    Pb.Var.x=logspace(-2,2,ini+n);
+    Pb.Var.npossflows = length(Pb.Var.x); % number of possible flowrates specified as data
+    Pb.Var.nvars = Pb.Var.nass+Pb.Var.nass*Pb.Var.npossflows+1; % number of total variables
+    
+    % Build tables of coolant properties and temperature
+    fprintf('\tBuilding coolant properties and temperature tables\n')
+    Coolant=coolant_properties('sodium',Input,Pb,Geometry);
+    
+    % Find which assemblies have adjacents and how many adjacent pairs
+    Input.adjacentAssemblies = findAdjacentAssemblies(Pb.Var.nass, Input.map, Input.lengthQ_original);
+    Input.nadj = nnz(Input.adjacentAssemblies); % number of adjacent assembly pairs
+    
+    % Read and create rings
+    Geometry.rings=find_rings(Input.adjacentAssemblies);
+    Geometry.nrings=100;
+    
+    % Create constraints
+    fprintf('*********************************************************************\n')
+    fprintf('Constraint matrices\n')
+    fprintf('*********************************************************************\n')
+    Pb=constraints_xi(Pb,Input,Coolant,Geometry);
+    
+    % Solve
+    fprintf('*********************************************************************\n')
+    fprintf('Solving with CPLEX\n');
+    fprintf('*********************************************************************\n')
+    [Solution.solutionvector, Solution.objval, Solution.status, Solution.output] = cplexmilp(Pb.CPLEX.c, Pb.CPLEX.Aineq, Pb.CPLEX.bineq, Pb.CPLEX.Aeq, Pb.CPLEX.beq, [], [], [], [], [], Pb.CPLEX.ctype,[],Pb.CPLEX.opts);
+    fprintf('exit status = % i\n', Solution.status);
+    fprintf('solution time = % f\n', Solution.output.time);
+    
+    % Post processing and check
+    if Solution.status==1
+        fprintf('*********************************************************************\n')
+        fprintf('SOLUTION FOUND : Post-processing and checking errors\n');
+        fprintf('*********************************************************************\n')
+        Output=xipp(Solution,Pb,Input,Coolant,Geometry);
+    else
+        Output={};
+        fprintf('\n**********************    NO SOLUTION FOUND    **********************\n\n')
+    end
+    
+    Results(n+1,:)={length(Pb.Var.x) Output Solution Solution.status};
+    n=n+1;
+    save(['../Solutions/' sol_name],'Results')
+end
+
+
+fprintf('*********************************************************************\n')
+fprintf('END OF SCRIPT\n');
+fprintf('*********************************************************************\n')
+diary off
+try
+    movefile clone* ./Clones/;
+end
